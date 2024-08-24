280469736b90d353e9e4cca8587880a5c3205101
Tee Wenjie <wenjietj@gmail.com>
2024-08-25 01:45:54 +0800

    add base and update path

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ import Stepper from './components/Stepper.vue';
 const data = ref(null)
 
 const fetchData = async () => {
-  const res = await fetch(`./data/content.json?v=${new Date().getTime()}`)
+  const res = await fetch(`portfolio-v2/data/content.json?v=${new Date().getTime()}`)
   data.value = await res.json()
 }
 
