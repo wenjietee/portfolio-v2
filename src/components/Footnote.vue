0ddd0b39a0991fb5e0ab10faec692a75f4cd96cb
Tee Wenjie <wenjietj@gmail.com>
2024-12-15 00:42:17 +0800

    create footnote and navbar

---
+++ src/components/Footnote.vue
@@ -0,0 +1,7 @@
+<template>
+    <section id="footnote">
+        <div class="container">
+            Footer
+        </div>
+    </section>
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+    <nav class="navbar">
+        <ul class="nav-list">
+            <li class="nav-item">
+                <a href="#home">
+                    <h5>Home</h5>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#about">
+                    <h5>About</h5>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#projects">
+                    <h5>Projects</h5>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#experience">
+                    <h5>Experience</h5>
+                </a>
+            </li>
+            <!-- <li class="nav-item">
+                    <a href="#blog">
+                        <h5>Blog</h5>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#game">
+                        <h5>Game</h5>
+                    </a>
+                </li> -->
+        </ul>
+    </nav>
+</template>
