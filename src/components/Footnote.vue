ef5fb67a075566bbc16897cd50e8657b41b7fe04
Tee Wenjie <wenjietj@gmail.com>
2024-12-15 00:46:35 +0800

    add footer details

--- src/components/Footnote.vue
+++ src/components/Footnote.vue
@@ -1,7 +1,7 @@
 <template>
     <section id="footnote">
         <div class="container">
-            Footer
+            Tee Wenjie ©{{ new Date().getFullYear() }}
         </div>
     </section>
 </template>
