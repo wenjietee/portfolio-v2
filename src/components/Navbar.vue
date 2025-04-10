4ab34af7b396dcb711466d1c9caf2e95da670f8a
Tee Wenjie <wenjietj@gmail.com>
2025-04-10 22:53:27 +0800

    add hamburger menu for mobile

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,51 @@
 <script setup>
+import { ref } from 'vue';
 import { animateNavText, resetNavText } from '../utils/animUtils';
+import Menu from 'primevue/menu';
+// import Button from 'primevue/button';
+
+const menu = ref();
+const items = ref([
+    {
+        items: [
+            {
+                label: 'Home',
+                url: '#home',
+            },
+            {
+                label: 'About',
+                url: '#about',
+            },
+            {
+                label: 'Projects',
+                url: '#projects',
+            },
+            {
+                label: 'Experience',
+                url: '#experience',
+            },
+            {
+                label: 'Contact',
+                url: '#contact',
+            }
+        ]
+    }
+]);
+
+const toggle = (event) => {
+    menu.value.toggle(event);
+};
+
 </script>
 
 <template>
     <nav class="navbar">
+        <div class="hamburger-menu">
+            <button type="button" class="hamburger" @click="toggle" aria-haspopup="true" aria-controls="overlay_menu">
+                <i class="pi pi-bars"></i>
+            </button>
+            <Menu ref="menu" id="overlay_menu" :model="items" :popup="true" />
+        </div>
         <ul class="nav-list">
             <li class="nav-item ">
                 <a href="#home">
