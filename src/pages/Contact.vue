7459b99b4606eadaf0a6d667489ca10f2f933b92
Tee Wenjie <wenjietj@gmail.com>
2025-04-13 11:46:27 +0800

    update tagline and contact layout

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -25,21 +25,22 @@ onBeforeUnmount(() => {
     <section id="contact">
 
         <div class="container grid">
-            <div class="col-12 text-6xl font-semibold text-center title" @mouseenter="animateTitleText"
+            <div class="col-12 contact-divider" />
+            <div class="col-12 text-6xl font-semibold text-center title contact-title" @mouseenter="animateTitleText"
                 @mouseleave="resetTitleText">
                 Contact
             </div>
-            <div class="xl:col-4 sm:col-12 text-left grid contact-description">
+            <div class="xl:col-3 sm:col-12 text-left grid contact-description">
                 <div class="col-12">{{ about.email }}</div>
                 <div class="col-12">{{ about.tagline }}</div>
             </div>
-            <div class="xl:col-4  sm:col-12 grid contact-copyright">
-                <div class="xl:col-6 sm:col-12">
-                    {{ about.name }} © {{ new Date().getFullYear() }}
-                </div>
-                <div class="xl:col-6 sm:col-12">{{ currentTime }} SGT(GMT+8)</div>
+
+            <div class="xl:col-3 sm:col-12 contact-copyright">
+                {{ about.name }} © {{ new Date().getFullYear() }}
             </div>
-            <div class="xl:col-4 sm:col-12">
+            <div class="xl:col-3 sm:col-12 contact-copyright">{{ currentTime }} SGT(GMT+8)</div>
+
+            <div class="xl:col-3 sm:col-12 contact-social-container">
                 <div class="contact-social-header">Socials</div>
                 <ul class="contact-social">
                     <li v-for="social in about.socials" :key="social.name">
