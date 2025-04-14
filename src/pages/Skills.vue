9b58f69665f7cb4bdc4c08d736416e083d8ab58f
Tee Wenjie <wenjietj@gmail.com>
2025-04-14 22:09:54 +0800

    format skills layout

--- src/pages/Skills.vue
+++ src/pages/Skills.vue
@@ -5,29 +5,29 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="skills-container">
-        <div class="grid">
-            <div class="col-12">
-                Logo Here
-            </div>
-            <div class="col-3">
-                [
-            </div>
-            <div class="col-3">
-                <div>Tech Skills</div>
-                <div v-for="techSkill in skills.tech_skills">
-                    <Chip :label="techSkill" class="chip" />
-                </div>
-            </div>
-            <div class="col-3">
-                <div>Art Skills</div>
-                <div v-for="artSkill in skills.art_skills">
-                    <Chip :label="artSkill" class="chip" />
-                </div>
-            </div>
-            <div class="col-3">
-                ]
+
+    <div class="grid skills-container">
+        <div class="col-12">
+            Logo Here
+        </div>
+        <div class="col-1 skills-bracket">
+            [
+        </div>
+        <div class="col-5 skills-list">
+            <h3>Tech</h3>
+            <div class="skills-about flex flex-wrap align-items-start justify-content-start gap-2">
+                <Chip v-for="techSkill in skills.tech_skills" :label="techSkill" class="chip" />
             </div>
         </div>
+        <div class="col-5 skills-list">
+            <h3>Art</h3>
+            <div class="skills-about flex flex-wrap align-items-start justify-content-end gap-2">
+                <Chip v-for="artSkill in skills.art_skills" :label="artSkill" class="chip" />
+            </div>
+        </div>
+        <div class="col-1 skills-bracket">
+            ]
+        </div>
     </div>
+
 </template>
